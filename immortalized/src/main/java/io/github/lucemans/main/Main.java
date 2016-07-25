f3ae19be8ca875aacf7b034a517adbd55f4f9833
jonas briers <jonas.briers@hotmail.com>
2016-07-25 12:44:13 +0200

    Merge

---
+++ immortalized/src/main/java/io/github/lucemans/main/Main.java
@@ -0,0 +1,20 @@
+package io.github.lucemans.main;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin implements Listener 
+{
+	@Override
+    public void onEnable()
+    {
+    	
+    }
+	
+	@Override
+	public void onDisable()
+	{
+		
+	}
+}
+
---
+++ immortalized/src/main/java/io/github/lucemans/vengefulone/Main.java
@@ -0,0 +1,12 @@
+package io.github.lucemans.vengefulone;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin implements Listener 
+{
+    public void onEnable()
+    {
+    	
+    }
+}
